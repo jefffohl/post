49938a6c90a6822a6a87d6b8e73243cdd2595399
jefffohl <jeff@fohl.com>
2015-12-01 17:57:33 -0800

    Moved them back.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-		define('CONFIG_PATH', '../../postConfig.php'); // the path and filename of your config file. This should be outside of the document root.
+		define('CONFIG_PATH', '../postConfig.php'); // the path and filename of your config file. This should be outside of the document root.
 		define('CONTROLLER', 'Post');
 
 		include('post/postLauncher.php');
